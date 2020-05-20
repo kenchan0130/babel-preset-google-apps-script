a16ffefd6b87007e0d50a7dbeb218cdcf035efde
Tadayuki Onishi <tt.tanishi100@gmail.com>
2020-05-21 01:26:32 +0900

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  plugins: ["prettier", "jest"],
+  extends: [
+    "eslint:recommended",
+    "plugin:security/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["prettier", "jest", "security"],
   env: {
     node: true,
     es6: true,
